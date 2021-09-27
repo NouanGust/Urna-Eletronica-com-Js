0387d3bf4d4d6d44f490d1ea8e3376b8ad64e464
Nohan Gustavo <81246430+NouanGust@users.noreply.github.com>
2021-09-27 15:33:44 -0300

    sistema funcionando

--- etapas.js
+++ etapas.js
@@ -4,7 +4,7 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: '38111',
                 nome: 'Mario',
                 partido: 'nintendo',
                 fotos: [
@@ -12,7 +12,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 77222,
+                numero: '77222',
                 nome: 'Sonic',
                 partido: 'Sega',
                 fotos: [
@@ -26,7 +26,7 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: '99',
                 nome : 'Tails',
                 partido :'Sega',
                 vice : 'Eggman',
@@ -36,7 +36,7 @@ let etapas = [
                 ]
             },
             {
-                numero: 84,
+                numero: '84',
                 nome: 'Luigi',
                 partido: 'Nintendo',
                 vice: 'Bowser',
--- script.js
+++ script.js
@@ -9,12 +9,15 @@ let numeros = document.querySelector('.d-1-3');
 // Variáveis de controle de ambiente
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
+let votos = [];
 
 // Funções
 function comecarEtapa(){
     let etapa = etapas[etapaAtual]
-
     let numeroHtml = '';
+    numero = '';
+    
 
     for(let i=0; i<etapa.numeros; i++){
         if(i === 0){
@@ -34,7 +37,35 @@ function comecarEtapa(){
 }
 
 function atualizaInterface(){
-
+   let etapa = etapas[etapaAtual];
+   let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero){
+            return true;
+        } else{
+            return false;
+        }
+   })
+   if(candidato.length > 0){
+       candidato = candidato [0];
+       seuVotoPara.style.display = 'block';
+       aviso.style.display = 'block';
+       descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
+       
+       let fotosHtml = '';
+       for(let i in candidato.fotos){
+           if(candidato.fotos[i].pequeno){
+                fotosHtml += `<div class="d-1-imagem pequeno"><img src="imagens/${candidato.fotos[i].url}" alt=""/> ${candidato.fotos[i].legenda}`
+           } else{
+                fotosHtml += `<div class="d-1-imagem"><img src="imagens/${candidato.fotos[i].url}" alt=""/> ${candidato.fotos[i].legenda}`
+           }
+            
+       }
+       lateral.innerHTML = fotosHtml;
+   } else{
+       seuVotoPara.style.display = 'block';
+       aviso.style.display = 'block';
+       descricao.innerHTML = '<div class= "aviso--grande pisca">VOTO NULO</div>';
+   }
 }
 
 function clicou(n){
@@ -42,19 +73,63 @@ function clicou(n){
     if(elNumero !== null){
         elNumero.innerHTML = n;
         numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca')
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca') 
+        } else{
+            atualizaInterface();
+        }
+        
+
     }
 }
 
 function branco(){
-    alert('Clicou em BRANCO')
+    if(numero === ''){
+         votoBranco = true;
+         seuVotoPara.style.display = 'block'
+         aviso.style.display = 'block'
+         numeros.innerHTML = '';
+         descricao.innerHTML = '<div class= "aviso--grande pisca">VOTO EM BRANCO</div>';
+         lateral.innerHTML = ''      
+    } else{
+        alert('Para votar em BRANCO você não pode ter digitado um número, porfavor CORRIJA e tente novamente')
+    }
 }
 
 function corrige(){
-    alert('Clicou em CORRIGE')
+    comecarEtapa();
 }
 
 function confirma(){
-    alert('Clicou em CONFIRMA')
+    let etapa = etapas[etapaAtual];
+
+    let votoConfirmado = false;
+
+    if(votoBranco === true) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    } else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if(votoConfirmado){
+        etapaAtual ++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else{
+            document.querySelector('.tela').innerHTML ='<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos) 
+        }
+    }
 }
 
 comecarEtapa();
