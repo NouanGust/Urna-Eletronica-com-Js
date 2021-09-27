70b913c56108bc3f8fec020a69d32af1c49fe796
Nohan Gustavo <81246430+NouanGust@users.noreply.github.com>
2021-09-27 09:34:07 -0300

    começo do js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Mario',
+                partido: 'nintendo',
+                fotos: [
+                    {url: 'mario-pres.jfif', legenda: 'Vereador', pequeno: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Sonic',
+                partido: 'Sega',
+                fotos: [
+                    {url: 'sonic-pres.jpg', legenda: 'Vereador', pequeno: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo : 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome : 'Tails',
+                partido :'Sega',
+                vice : 'Eggman',
+                fotos:[
+                    {url:'tails-vice.jfif', legenda: 'Prefeito'},
+                    {url:'doc-vice.jpg', legenda: 'Vice-Prefeito', pequeno: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Luigi',
+                partido: 'Nintendo',
+                vice: 'Bowser',
+                fotos:[
+                    {url:'luigi-vice.jfif', legenda:'Prefeito'},
+                    {url: 'bowser-vice.jfif', legenda:'Vice-Prefeito', pequeno:true}
+                ]
+            }
+        ]
+    }
+]
